c64ab5ccf8abdb17b6c68b7cf4d4bf97d59d7f84
Rachel Braverman <rachelb@shfing.com>
2024-01-15 17:39:38 +0200

    initail

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Ruler from './components/Ruler';
+
+function App() {
+  return (
+    <div >
+      <Ruler/>
+    </div>
+  );
+};
+
+
+export default App
---
+++ src/components/Ruler.tsx
@@ -0,0 +1,7 @@
+import "../App.css";
+
+const Ruler = () => {
+  return <div>numbers Ruler</div>;
+};
+
+export default Ruler;
