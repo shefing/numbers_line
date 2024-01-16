4950b695f11af6e74c8d28118b66b85c3da55520
Tsemach Hadad <tsemachh@gmail.com>
2024-01-16 12:29:18 +0200

    Inital Ruler

--- src/components/Axis.tsx
+++ src/components/Axis.tsx
@@ -1,6 +1,6 @@
 const Axis = ({ labels }) => {
     return (
-        <div className="flex justify-between border-t-2 border-gray-200 pt-2 mx-4">
+        <div className="flex justify-between border-t-2 border-gray-200 pt-0 mx-4">
             {labels.map((label, index) => (
                 <div key={index} className="text-sm text-gray-500 flex flex-col items-center">
                     <div className="h-2 border-l-2 border-gray-200 w-px"></div>
