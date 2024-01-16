5961006d43eb5fe53534566d5ecd2a15dcdb2c62
Rachel Braverman <rachelb@shfing.com>
2024-01-16 15:02:02 +0200

    first ruller commit

--- src/components/Ruler.tsx
+++ src/components/Ruler.tsx
@@ -1,19 +1,13 @@
-import "../App.css";
-
-const Ruler = ({}) => {
-  // const xAxisData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // return axis(d3.scaleLinear()).ticks(10).render();
-  const labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+import XAxis from "./ruler/XAxis";
+const Ruler = () => {
+  const labels = Array.from({ length: 21 }, (_, index) => index);
   return (
-    <div className="flex justify-between border-t-2 border-gray-200 pt-2">
-      {labels.map((label, index) => (
-        <div key={index} className="text-sm text-gray-500">
-          <div className="h-2 border-l-2 border-gray-200"></div>
-          {label}
-        </div>
-      ))}
+    <div>
+      {" "}
+      <XAxis labels={labels} />
     </div>
   );
 };
 
 export default Ruler;
+// enum
