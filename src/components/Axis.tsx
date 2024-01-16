7ad864e894a118bb92e7354708c89f109c72d706
Tsemach Hadad <tsemachh@gmail.com>
2024-01-16 12:04:31 +0200

    Inital Ruler

---
+++ src/components/Axis.tsx
@@ -0,0 +1,14 @@
+const Axis = ({ labels }) => {
+    return (
+        <div className="flex justify-between border-t-2 border-gray-200 pt-2 mx-4">
+            {labels.map((label, index) => (
+                <div key={index} className="text-sm text-gray-500 flex flex-col items-center">
+                    <div className="h-2 border-l-2 border-gray-200 w-px"></div>
+                    {label}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Axis;
--- src/components/Ruler.tsx
+++ src/components/Ruler.tsx
@@ -1,7 +1,9 @@
 import "../App.css";
-
+import { Button } from "@/components/ui/button"
+import Axis from "./Axis"
 const Ruler = () => {
-  return <div>numbers Ruler</div>;
+  return <div><Axis labels={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} /></div>
 };
 
 export default Ruler;
+
