4044f2485c54630ba0867ef85bb5a45e1569861a
sarasla <sarahs@shefing.com>
2024-01-18 10:37:25 +0200

    fix height

--- src/components/Ruler.tsx
+++ src/components/Ruler.tsx
@@ -2,7 +2,7 @@ import XAxis from "./ruler/XAxis";
 const Ruler = () => {
   const labels = Array.from({ length: 11 }, (_, index) => index);
   return (
-    <div className="absolute w-full bottom-[20%] left-0">
+    <div className="absolute w-full bottom-[26%] left-0">
       <XAxis labels={labels} />
     </div>
   );
