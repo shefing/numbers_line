10221cc0fbb08865a5bb07be52d60b8465bc04f9
Rachel Braverman <rachelb@shfing.com>
2024-01-21 12:40:32 +0200

    fix4

--- src/components/ruler/XAxis.tsx
+++ src/components/ruler/XAxis.tsx
@@ -7,47 +7,19 @@ interface IProps {
 }
 const XAxis = ({ labels }: IProps) => {
   const [startIndex, setStartIndex] = useState(0);
-  const [isDragging, setIsDragging] = useState(false);
 
-  const handleMouseDown = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setIsDragging(true);
-  };
-
-  const handleMouseMove = (e: React.MouseEvent) => {
-    e.preventDefault();
-    if (isDragging) {
-      const mouseX = e.clientX;
-      const axisRect = e.currentTarget.getBoundingClientRect();
-      const axisWidth = axisRect.width;
-      const relativePosition = mouseX - axisRect.left;
-      const labelIndex = Math.floor((relativePosition / axisWidth) * labels.length);
-      setStartIndex(Math.min(80, Math.max(0, labelIndex)));
-    }
-  };
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
-  };
   return (
     <>
       {labels.length == 101 ? (
         <>
           <Arrows startIndex={startIndex} setStartIndex={setStartIndex} />
-          <div
-            className="stroke-3 stroke-var-black fixed left-0 right-0 flex justify-between border-t-2 border-gray-900 pt-0 mx-0 items-center pl-8 pr-8 "
-            onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-            onMouseUp={handleMouseUp}
-            onMouseLeave={handleMouseUp}
-            style={{ cursor: isDragging ? "grabbing" : "grab" }}
-          >
-            <Numbers labels={labels} startIndex={startIndex} />
+          <div className="stroke-3 stroke-var-black fixed left-0 right-0 flex justify-between border-t-2 border-gray-900 pt-0 mx-0 items-center pl-8 pr-8 ">
+            <Numbers labels={labels} startIndex={startIndex} setStartIndex={setStartIndex} />
           </div>
         </>
       ) : (
         <div className="fixed left-0 right-0 flex justify-between border-t-2 border-gray-900 pt-0 mx-0 items-center pl-8 pr-8 ">
-          <Numbers labels={labels} startIndex={startIndex} />
+          <Numbers labels={labels} startIndex={startIndex} setStartIndex={setStartIndex} />
         </div>
       )}
     </>
