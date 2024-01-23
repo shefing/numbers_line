671cafc38ce39e0e7eeb08299b4338bfa05fb6b5
Rachel Braverman <rachelb@shfing.com>
2024-01-23 12:20:26 +0200

    fix build

--- src/components/ruler/Numbers.tsx
+++ src/components/ruler/Numbers.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNumbersLineContext } from "../../context/numbersLineContext";
 import { LineRange } from "../../type/Line";
 
