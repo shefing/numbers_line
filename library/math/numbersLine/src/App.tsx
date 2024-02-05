9e874d877d4d2f988278241d47f3fad9eaf732a0
Rachel Braverman <rachelb@shfing.com>
2024-02-05 15:04:29 +0200

    fix review

--- library/math/numbersLine/src/App.tsx
+++ library/math/numbersLine/src/App.tsx
@@ -3,25 +3,8 @@ import Ruler from "./components/Ruler";
 import GrassImg from "./components/GrassImg";
 import Toolbar from "./components/Toolbar";
 import ShowElements from "./components/ShowElements";
-import { useNumbersLineContext } from "./context/numbersLineContext";
-import { useEffect } from "react";
 
 const App = () => {
-  const { setIdDraggElementClick } = useNumbersLineContext();
-
-  const handleClickOutside = (event: any) => {
-    if (!event.target.classList.contains("dragElement")) {
-      false && setIdDraggElementClick(-1);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, []);
-
   return (
     <div className="flex flex-col h-full justify-between">
       <Toolbar />
--- library/math/numbersLine/src/components/ShowElements.tsx
+++ library/math/numbersLine/src/components/ShowElements.tsx
@@ -6,13 +6,7 @@ const ShowElements = () => {
   const { dragElements, setIdDraggElementClick } = useNumbersLineContext();
 
   return dragElements.map((element) => (
-    <div
-      className="dragElement"
-      key={element.id}
-      onClick={() => {
-        setIdDraggElementClick(element.id);
-      }}
-    >
+    <div className="dragElement" key={element.id} onClick={() => setIdDraggElementClick(element.id)}>
       {element.type == TypesElement.jump && <Jump element={element} />}
     </div>
   ));
