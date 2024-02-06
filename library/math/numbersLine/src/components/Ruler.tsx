cce8f43e38c03ab820b823fe3ea3e0b2ebf665e6
Rachel Braverman <rachelb@shfing.com>
2024-02-06 11:25:51 +0200

    change2

--- library/math/numbersLine/src/components/Ruler.tsx
+++ library/math/numbersLine/src/components/Ruler.tsx
@@ -14,7 +14,7 @@ const Ruler = () => {
   }, [windowWidth]);
 
   return (
-    <div className="ruler pb-[20%]">
+    <div className="ruler pb-[15%]">
       <Arrows leftPosition={leftPosition} setLeftPosition={setLeftPosition} />
       <XAxis leftPosition={leftPosition} setLeftPosition={setLeftPosition} />
     </div>
