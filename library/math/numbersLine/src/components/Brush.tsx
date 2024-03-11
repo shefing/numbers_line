777a7b8515ff69c7bc4d35074578bda16a5a609f
Rachel Braverman <rachelb@shfing.com>
2024-03-11 14:20:59 +0200

    delete fabric

--- library/math/numbersLine/src/components/Brush.tsx
+++ library/math/numbersLine/src/components/Brush.tsx
@@ -1,55 +1,45 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { fabric } from "fabric";
 import { useNumbersLineContext } from "../context/numbersLineContext";
 
 const Brush = () => {
   const { windowSize, color } = useNumbersLineContext();
   const canvasRef = useRef(null);
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [isErasing, setIsErasing] = useState(false); // State to track erase mode
+  let isDrawing = false;
   let brush: any;
 
   useEffect(() => {
     const canvas = new fabric.Canvas(canvasRef.current, {
-      isDrawingMode: color !== "" && !isErasing, // Enable drawing mode only if color is selected and erase mode is not active
+      isDrawingMode: true,
     });
     canvas.freeDrawingBrush.width = 10;
     canvas.freeDrawingBrush.color = color;
     brush = canvas.freeDrawingBrush;
-
     return () => {
-      canvas.dispose();
+      canvas.dispose(); // Clean up Fabric.js canvas when component unmounts
     };
-  }, [color, isErasing]);
+  }, [color]);
 
   const handleMouseDown = () => {
-    setIsDrawing(true);
-  };
-  const handleMouseMove = () => {
-    if (!isDrawing) return;
-    // If erase mode is active, remove the existing strokes at the mouse pointer
-    const canvas: any = canvasRef.current;
-    if (isErasing && canvas) {
-      const pointer = canvas.getPointer(canvasRef.current);
-      canvas.getObjects().forEach((obj: any) => {
-        if (obj.containsPoint(pointer)) {
-          canvas.remove(obj);
-        }
-      });
-    }
-  };
-  const handleMouseUp = () => {
-    setIsDrawing(false);
+    isDrawing = true;
   };
 
-  const toggleEraseMode = () => {
-    setIsErasing((prev) => !prev); // Toggle erase mode
+  const handleMouseMove = () => {
+    if (!isDrawing) return;
+    //   const pointer = canvasRef??canvasRef.current.getPointer(event.e);
+    //   brush.onMouseDown(pointer);
   };
+
+  const handleMouseUp = () => {
+    isDrawing = false;
+  };
+
   return (
-    <div>
-      <canvas ref={canvasRef} width={windowSize.width} height={100} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} />
-      <button onClick={toggleEraseMode}>Toggle Erase Mode</button> {/* Button to toggle erase mode */}
-    </div>
+    <>
+      {color != "" && (
+        <canvas ref={canvasRef} width={windowSize.width} height={100} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} />
+      )}
+    </>
   );
 };
 
