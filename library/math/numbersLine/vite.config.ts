08d9cf9cf2ca352360071266f6a2a316bd363595
Tsemach Hadad <tsemachh@gmail.com>
2024-04-03 09:37:36 +0300

    1 file change

--- library/math/numbersLine/vite.config.ts
+++ library/math/numbersLine/vite.config.ts
@@ -1,16 +1,23 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
  
   },
+  build: {
+    assetsInlineLimit: Number.MAX_SAFE_INTEGER,
+    // sourcemap: "inline"
+  },
   server: {
     port: 3000,
-  },
+  }
 })
+
+
