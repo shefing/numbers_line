1a509197b15801149b215f5472196cc7284a6dfd
Rachel Braverman <rachelb@shfing.com>
2024-04-04 13:02:48 +0300

    merge

--- library/math/numbersLine/src/components/showElements/Text.tsx
+++ library/math/numbersLine/src/components/showElements/Text.tsx
@@ -1,57 +1,127 @@
 import { IElement } from "../../type/moveable";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import MoveableElement from "./MoveableElement";
 import { useNumbersLineContext } from "../../context/numbersLineContext";
 import { useDraggableElementAction } from "../../hooks/useDraggableElementAction";
-import { dragElementID, maxTextBoxSize, textBoxSize } from "../../consts/elementConsts";
-import { calcXTransform } from "../../lib/utils";
+import { dragElementID, keboardActioKeys, keboardDifferentlButton, keboardLayers, keboardNormalButtons } from "../../consts/elementConsts";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 
 interface IProps {
   element: IElement;
 }
 
 const Text = ({ element }: IProps) => {
-  const { windowSize, idDraggElementClick, setIdDraggElementClick } = useNumbersLineContext();
-  const { deleteDragElement, updateDragElements, updateDragElementsLayers } = useDraggableElementAction();
-  const [size, setSize] = useState(textBoxSize);
+  const { idDraggElementClick, setIdDraggElementClick } = useNumbersLineContext();
+  const { deleteDragElement, updateDragElementsLayers } = useDraggableElementAction();
   const [dragging, setDragging] = useState(false);
+  const [openKeyboard, setOpenKeyboard] = useState(true);
   const moveableRef = useRef<any>(null);
+  const textInput = useRef<any>();
 
-  const updateValueAndSize = (e: any) => {
-    updateDragElementsLayers(element);
-    setIdDraggElementClick("");
-    setSize(e.target.value.length > textBoxSize ? (e.target.value.length > maxTextBoxSize ? maxTextBoxSize : e.target.value.length) : textBoxSize);
-    const inputWidth = e.target.offsetWidth;
-    const startPosition = calcXTransform(element.transform);
-    const endPosition = startPosition + inputWidth;
-    const outOfRange = windowSize.width - endPosition;
-    if (outOfRange < 0) {
-      const newStartPosition = startPosition + outOfRange;
-      const newTransform = element.transform.replace("(" + startPosition, "(" + newStartPosition);
-      updateDragElements(element.id, { ...element, transform: newTransform });
-      e.target.style.transform = newTransform;
+  useEffect(() => {
+    const closeKeyboard = (event: any) => {
+      !moveableRef.current.contains(event.target) && setOpenKeyboard(false);
+    };
+    const openKeyboard = (event: any) => {
+      textInput.current.contains(event.target) && setOpenKeyboard(true);
+    };
+    document.addEventListener("mousedown", closeKeyboard);
+    document.addEventListener("dblclick", openKeyboard);
+    return () => {
+      document.removeEventListener("mousedown", closeKeyboard);
+      document.removeEventListener("dblclick", openKeyboard);
+    };
+  }, []);
+
+  useEffect(() => {
+    openKeyboard ? textInput.current?.focus() : textInput.current?.blur();
+  }, [openKeyboard]);
+
+  const updateValue = (v: string) => {
+    let startIndex = textInput.current.selectionStart;
+    const endIndex = textInput.current.selectionEnd;
+    if (v === "⌫") {
+      if (startIndex == endIndex) startIndex -= 1;
+      textInput.current.value = textInput.current.value.substring(0, startIndex) + textInput.current.value.substring(endIndex);
+    } else {
+      textInput.current.value = textInput.current.value.substring(0, startIndex) + v + textInput.current.value.substring(endIndex);
+      startIndex += 1;
+    }
+    textInput.current.setSelectionRange(startIndex, startIndex);
+  };
+  const onKeyDown = (e: any) => {
+    if (!(keboardLayers.some((item) => item.includes(e.key)) || keboardActioKeys.includes(e.key))) {
+      e.preventDefault();
+      return;
     }
   };
 
-  const deleteIfEmpty = (e: { target: { value: string } }) => {
-    !e.target.value.length && deleteDragElement(element.id);
+  const onChange = () => {
+    updateDragElementsLayers(element);
+    setIdDraggElementClick("");
+    textInput.current.style.height = textInput.current.scrollHeight + "px";
+  };
+
+  const onBlur = (e: { target: { value: string } }) => {
+    openKeyboard ? textInput.current?.focus() : textInput.current?.blur();
+    !e.target.value.length && !openKeyboard && deleteDragElement(element.id);
+  };
+
+  const onKeyPress = (button: string, event: any) => {
+    if (event.which && event.which == 3) {
+      event.preventDefault();
+      return;
+    }
+    updateValue(button == "⏎" ? "\n" : button);
   };
   return (
     <>
-      <input
+      <div
+        id={`dragElement-keyboardCET-${element.id}`}
         ref={moveableRef}
-        id={`dragElement-${element.id}`}
-        type="text"
-        size={size}
-        onChange={updateValueAndSize}
-        onBlur={deleteIfEmpty}
-        className={`drag-element text-box ${dragging && "outline-none border-[1.5px] border-[#009FDE]"}`}
-        autoFocus
+        className={`drag-element ${openKeyboard ? "flex-col bg-white rounded-[6px] border border-[#009FDE] p-1.5 mt-[-1.5] " : ""}`}
         style={{
           transform: element.transform,
           zIndex: element.zIndex,
         }}
-      />
+      >
+        <textarea
+          ref={textInput}
+          id={`dragElement-${element.id}`}
+          autoFocus
+          onKeyDown={(e) => onKeyDown(e)}
+          onChange={onChange}
+          onBlur={onBlur}
+          className={`text-box max-h-50${dragging && "outline-none border-[1.5px] border-[#009FDE]"}`}
+          style={{
+            zIndex: element.zIndex,
+            resize: "none",
+            overflowY: "auto",
+            wordWrap: "break-word",
+          }}
+        />
+        {openKeyboard && (
+          <Keyboard
+            onKeyPress={(input, event) => onKeyPress(input, event)}
+            onChange={onChange}
+            layout={{
+              default: keboardLayers,
+            }}
+            theme={"hg-theme-default hg-layout-default keyboard-background"}
+            buttonTheme={[
+              {
+                class: "keyboard-buttons keyboard-buttons-enter",
+                buttons: keboardDifferentlButton,
+              },
+              {
+                class: "keyboard-buttons",
+                buttons: keboardNormalButtons,
+              },
+            ]}
+          />
+        )}
+      </div>
       {idDraggElementClick == element.id && (
         <div id={`${dragElementID}-text`}>
           <MoveableElement moveableRef={moveableRef} element={element} unit={0} dragging={dragging} setDragging={setDragging} />
