f5476eab1cf87568d290b1dc0e8e24e0019afed2
Tsemach Hadad <tsemachh@gmail.com>
2024-04-04 16:20:51 +0300

    Arabic support

--- library/math/numbersLine/src/context/numbersLineContext.tsx
+++ library/math/numbersLine/src/context/numbersLineContext.tsx
@@ -12,7 +12,7 @@ if (locale) i18n.changeLanguage(locale);
 if (!locale) {
   locale = ILanguage.HE;
 }
-
+console.log("lcoale",locale);
 interface INumbersLineContextProps {
   language: ILanguage;
   windowSize: IWindowSize;
