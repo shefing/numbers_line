749ea162cfa02fcb4123026451d42a5118c58e45
Rachel Braverman <rachelbraverman@MacBooks-MacBook-Pro.local>
2024-07-09 12:34:29 +0300

    little change

--- library/math/numbersLine/src/components/Ruler.tsx
+++ library/math/numbersLine/src/components/Ruler.tsx
@@ -1,10 +1,12 @@
 import Arrows from "./ruler/Arrows";
 import XAxis from "./ruler/XAxis";
 import { rulerLocation } from "../consts/elementConsts";
+import { useNumbersLineContext } from "@/context/numbersLineContext";
 
 const Ruler = () => {
+  const { windowSize } = useNumbersLineContext();
   return (
-    <div style={{ position: "absolute", bottom: rulerLocation * 100 + "%", width: "100%" }}>
+    <div style={{ position: "absolute", bottom: rulerLocation * windowSize.height, width: "100%" }}>
       <Arrows />
       <XAxis />
     </div>
