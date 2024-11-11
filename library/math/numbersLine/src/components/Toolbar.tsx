206a96bdc39df97404228906211a7c8b7ab13ac0
Rachel Braverman <rachelbraverman@MacBooks-MacBook-Pro.local>
2024-11-11 11:01:48 +0200

    remove full screan button

--- library/math/numbersLine/src/components/Toolbar.tsx
+++ library/math/numbersLine/src/components/Toolbar.tsx
@@ -1,5 +1,4 @@
 import LineDefinition from "./toolbar/LineDefinition";
-import fullscreen from "/assets/icons/toolbar/fullscreen.svg";
 import IconsToolbar from "./toolbar/IconsToolbar";
 import { ToolbarHeight, actionIconsDetials } from "../consts/elementConsts";
 import { IActionIcon } from "../type/toolbar";
@@ -7,7 +6,7 @@ import { IActionIcon } from "../type/toolbar";
 const Toolbar = () => {
   return (
     <div className={`flex justify-between w-full bg-[#ECF9FF] z-[999]`} style={{ height: ToolbarHeight + "px" }}>
-      <img className="p-5 flex-none" src={fullscreen} alt="Fullscreen Toolbar" />
+      <div />
       <div className="flex w-[456px] ml-[4.25rem]">
         {actionIconsDetials.map((item: IActionIcon, i: number) => (
           <IconsToolbar key={i} typeAction={item.type} iconUrl={item.url} isDragged={item.isDragged} isMenu={item.isMenu} />
